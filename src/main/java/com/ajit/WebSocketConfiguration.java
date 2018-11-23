cb32f0cabcc88a86c16fdc9288aeb370cfeaecc7
Ajit Singh <ajit.singh@jambit.com>
2018-11-23 22:11:01 +0100

    fix ws channel

--- src/main/java/com/ajit/WebSocketConfiguration.java
+++ src/main/java/com/ajit/WebSocketConfiguration.java
@@ -19,6 +19,6 @@ public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.setApplicationDestinationPrefixes("/app")
-                .enableSimpleBroker("/chat");
+                .enableSimpleBroker("/gifs");
     }
 }
